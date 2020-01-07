c32ba6f85770a790306508f7c3dbc98d01de9537
Yash <yash.talegaonkar37@gmail.com>
2020-01-07 09:39:27 +0530

    (3)
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

--- src/app/database-service.service.ts
+++ src/app/database-service.service.ts
@@ -1,29 +1,34 @@
+
 import { Injectable } from '@angular/core';
-require('src/server');
-import { from } from 'rxjs';
-
-
-export const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ac0de52dh',
-  database: 'electron'
-});
-
-connection.connect();
+import * as mysql from 'mysql';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseServiceService {
 
+  constructor() {
 
-  // connection.connect();
-
-
-  constructor() { }
+  }
 
   db() {
-    console.log();
+    const dbconn = mysql.createConnection({
+      host: 'localhost',
+          user: 'root',
+          password: 'ac0de52dh',
+          database: 'electron'
+    });
+
+    dbconn.connect();
+    dbconn.query('CREATE TABLE IF NOT EXISTS contacts(id INT, name VARCHAR(50), email VARCHAR(100))',
+      (err) => {
+        if (err) {
+          console.log('Error!', err);
+        } else {
+          console.log('sucess');
+        }
+      }
+    );
   }
+
 }
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,36 @@
 
-const mysql = require('mysql2');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ac0de52dh',
-    database: 'electron'
-});
-
-connection.connect();
-
-connection.query('CREATE TABLE IF NOT EXISTS contacts(id INT, name VARCHAR(50), email VARCHAR(100))',
-    (err) => {
-        if (err) {
-            console.log("Error!", err);
-        } else {
-            console.log("sucess");
-        }
-    }
-);
-
-// const query = '';
-
-connection.query('INSERT INTO contacts (name, email) VALUES ("sad", "name001@email.com")', (err, res) => {
-    if (err) { console.log(err); }
-});
-
-
-// addcontact(query);
-// connection.query('SELECT * FROM contacts', (err, rows) => {
-//     if (err) { console.log(err); }
-
-//     console.log(rows);
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'ac0de52dh',
+//     database: 'electron'
 // });
+
+// connection.connect();
+
+// connection.query('CREATE TABLE IF NOT EXISTS contacts(id INT, name VARCHAR(50), email VARCHAR(100))',
+//     (err) => {
+//         if (err) {
+//             console.log('Error!', err);
+//         } else {
+//             console.log('sucess');
+//         }
+//     }
+// );
+
+// // const query = '';
+
+// connection.query('INSERT INTO contacts (name, email) VALUES ("sad", "name001@email.com")', (err, res) => {
+//     if (err) { console.log(err); }
+// });
+
+
+// // addcontact(query);
+// // connection.query('SELECT * FROM contacts', (err, rows) => {
+// //     if (err) { console.log(err); }
+
+// //     console.log(rows);
+// // });
 
