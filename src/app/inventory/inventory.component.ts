9c86eb8512c87ce39c7a1091bafdb8ef86f9f1b1
Yash <yash.talegaonkar37@gmail.com>
2020-02-17 15:42:43 +0530

    inventory created
    Signed-off-by: Yash <yash.talegaonkar37@gmail.com>

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
+  srno = 'SR NO';
+  invname = 'Inventory Names';
+  adduser = 'Add Inventory / Delete Inventory ';
+
+
+  inventory; // Inventory Object
   constructor() { }
 
   ngOnInit() {
